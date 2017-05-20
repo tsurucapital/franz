2327fd6c918cb2fab665ddf5713b0ee3a74b2cdc
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-05-20 21:10:56 +0900

    rename read to subscribe

--- app/Client.hs
+++ app/Client.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE LambdaCase, RecordWildCards #-}
 module Main where
 
+import Control.Exception
 import Control.Monad
 import Database.Liszt.Client
+import Data.Function (fix)
 import qualified Data.ByteString.Char8 as B
 import System.Environment
 import System.IO
+import System.IO.Error (isEOFError)
 
 parseHostPort :: String -> (String -> Int -> r) -> r
 parseHostPort str k = case break (==':') str of
@@ -14,11 +17,20 @@ parseHostPort str k = case break (==':') str of
 
 main :: IO ()
 main = getArgs >>= \case
-  ["read", hostPort] -> parseHostPort hostPort withConsumer $ \conn -> forever $ do
+  ["subscribe", hostPort] -> parseHostPort hostPort withConsumer $ \conn -> forever $ do
     bs <- readBlocking conn
     B.hPut stdout bs
     putStrLn ""
-  ["write", hostPort] -> parseHostPort hostPort withProducer $ \conn -> forever $ do
+  ["read", hostPort] -> parseHostPort hostPort withConsumer $ \conn -> fix
+    $ \self -> readNonBlocking conn >>= \case
+      Just bs -> do
+        B.hPut stdout bs
+        putStrLn ""
+        self
+      Nothing -> return ()
+  ["write", hostPort] -> parseHostPort hostPort withProducer
+    $ \conn -> handle (\e -> if isEOFError e then return () else throwIO e)
+    $ forever $ do
     bs <- B.getLine
     writeSeqNo conn bs
   _ -> fail "Usage: liszt read host:port\nliszt write host:port"
