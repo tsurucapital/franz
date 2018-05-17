58387ee1e7449d2daa15952af73518f615c2ae37
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-05-17 14:44:57 +0900

    make the command-line client more flexible

--- app/client.hs
+++ app/client.hs
@@ -10,20 +10,64 @@ import qualified Data.ByteString.Char8 as B
 import System.Environment
 import System.IO
 import System.IO.Error (isEOFError)
+import System.Console.GetOpt
+import System.Exit
 
 parseHostPort :: String -> (String -> Int -> r) -> r
 parseHostPort str k = case break (==':') str of
   (host, ':' : port) -> k host (read port)
   (host, _) -> k host 1886
 
-main :: IO ()
-main = getArgs >>= \case
-  ["read", hostPort, name, begin, end] -> parseHostPort hostPort withConnection $ \conn -> do
-    bss <- fetch conn (Request (B.pack name) 1000000 (read begin) (read end))
-    mapM_ (B.hPutStrLn stdout) bss
-  ["write", path] -> withWriter path
-    $ \h -> handle (\e -> if isEOFError e then return () else throwIO e)
-    $ forever $ do
-      bs <- B.getLine
-      write h Proxy bs
-  _ -> fail "Usage: liszt read host:port\nliszt write host:port"
+data Options = Options
+  { host :: String
+  , timeout :: Double
+  , ranges :: [(Int, Int)]
+  , beginning :: Maybe Int
+  }
+
+readOffset :: String -> Int
+readOffset ('_' : n) = -read n
+readOffset n = read n
+
+options :: [OptDescr (Options -> Options)]
+options = [Option "h" ["host"] (ReqArg (\str o -> o { host = str }) "HOST:PORT") "stream input"
+  , Option "r" ["range"] (ReqArg (\str o -> o { ranges = case break (==':') str of
+      (begin, ':' : end) -> (readOffset begin, readOffset end) : ranges o
+      _ -> (readOffset str, readOffset str) : ranges o
+      }) "FROM:TO") "ranges"
+  , Option "b" ["begin"] (ReqArg (\str o -> o { beginning = Just $! readOffset str }) "pos") "get all the contents from this position"
+  , Option "t" ["timeout"] (ReqArg (\str o -> o { timeout = read str }) "SECONDS") "Timeout"
+  ]
+
+defaultOptions :: Options
+defaultOptions = Options
+  { host = "localhost"
+  , timeout = 1e12
+  , ranges = []
+  , beginning = Nothing
+  }
+
+printBS :: B.ByteString -> IO ()
+printBS bs = do
+  print $ B.length bs
+  B.hPutStr stdout bs
+  hFlush stdout
+
+main = getOpt Permute options <$> getArgs >>= \case
+  (fs, name : _, []) -> do
+    let o = foldl (flip id) defaultOptions fs
+    parseHostPort (host o) withConnection $ \conn -> do
+      let name' = B.pack name
+      let timeout' = floor $ timeout o * 1000000
+      let req = Request name' timeout'
+      forM_ (reverse $ ranges o) $ \(i, j) -> do
+        bss <- fetch conn $ req i j
+        mapM_ printBS bss
+      forM_ (beginning o) $ \start -> flip fix start $ \self i -> do
+        bss <- fetch conn $ req i i
+        mapM_ printBS bss
+        unless (null bss) $ self $ i + 1
+
+  (_, _, es) -> do
+    name <- getProgName
+    die $ unlines es ++ usageInfo name options
